facd736e75ebe188b4deb64cf3742021a696fed8
aschendel <aschendel@localhost.localdomain>
2025-03-17 16:22:07 -0500

    fixed data.tf

--- xcp/n8nserver/data.tf
+++ xcp/n8nserver/data.tf
@@ -1,7 +1,7 @@
 data "xenorchestra_template" "template" {
   name_label = "Ubuntu Jammy Jellyfish 22.04"
 }
-data "xenorchestra_pool" "9J5" {
+data "xenorchestra_pool" "testhost" {
     name_label = "xcp-ng-9J5CXG2"
 }
 data "xenorchestra_network" "net" {
