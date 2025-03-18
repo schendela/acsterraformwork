56a9d630fa829014d61ad3dccc4bd8d1635037c9
aschendel <aschendel@localhost.localdomain>
2025-03-18 16:47:05 -0500

    added pool id to template

--- xcp/n8nserver/data.tf
+++ xcp/n8nserver/data.tf
@@ -1,5 +1,6 @@
 data "xenorchestra_template" "ubuntu" {
   name_label = "Ubuntu Jammy Jellyfish 22.04"
+  pool_id = data.xenorchestra_pool.testhost.id
 }
 data "xenorchestra_pool" "testhost" {
     name_label = "xcp-ng-9J5CXG2"
