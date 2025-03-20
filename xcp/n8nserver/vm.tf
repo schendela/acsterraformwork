ea485759d35db41c085eab67262c3bd10a4940a7
aschendel <aschendel@rl9dev.grapelandisd.local>
2025-03-20 14:41:55 -0500

    data and vm updates. To use template

--- xcp/n8nserver/vm.tf
+++ xcp/n8nserver/vm.tf
@@ -6,9 +6,12 @@ resource "xenorchestra_vm" "n8n" {
     name_description = "Ubuntu server. Running docker. N8N Server"
     template = data.xenorchestra_template.ubuntu.id
  #   cdrom = data.xenorchestra_vdi.vdi.id
-    cdrom {
-	id = data.xenorchestra_vdi.vdi.id
-}
+ #   cdrom {
+#	id = data.xenorchestra_vdi.vdi.id
+#}
+    provisioner "local-exec" {
+	command = "sudo hostnamectl set-hostname n8n.grapelandisd.local"
+    }
     # Prefer to run the VM on the primary pool instance
     affinity_host = data.xenorchestra_pool.testhost.master
     network {
@@ -16,7 +19,7 @@ resource "xenorchestra_vm" "n8n" {
     }
     disk {
       sr_id = "6f710bd3-cd24-d6ac-f7e9-8c076239172f"
-      name_label = "n8nssd"
+      name_label = "n8ntest"
       size = 64424509440
     }
 
