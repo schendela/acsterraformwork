a39498d62268d904f4dc216c9026771a59c59e15
aschendel <aschendel@rl9dev.grapelandisd.local>
2025-03-20 21:19:25 -0500

    n8n test server. New base template

---
+++ xcp/n8ntestserver/data.tf
@@ -0,0 +1,14 @@
+data "xenorchestra_template" "ubuntu" {
+  name_label = "base-image"
+  pool_id = data.xenorchestra_pool.testhost.id
+}
+data "xenorchestra_pool" "testhost" {
+    name_label = "xcp-ng-9J5CXG2"
+}
+data "xenorchestra_network" "net" {
+  name_label = "VLAN64"
+}
+#data "xenorchestra_vdi" "vdi" {
+ # name_label = "ubuntu-24.04.1-live-server-amd64.iso"
+ # pool_id = data.xenorchestra_pool.testhost.id
+#}
---
+++ xcp/n8ntestserver/vm.tf
@@ -0,0 +1,35 @@
+resource "xenorchestra_vm" "n8n" {
+    memory_max = 8589934592
+    cpus  = 2
+    #cloud_config = xenorchestra_cloud_config.bar.template
+    name_label = "n8n-test-server"
+    name_description = "Ubuntu server. Running docker. N8N Server"
+    template = data.xenorchestra_template.ubuntu.id
+ #   cdrom = data.xenorchestra_vdi.vdi.id
+ #   cdrom {
+#	id = data.xenorchestra_vdi.vdi.id
+#}
+    provisioner "local-exec" {
+	command = "sudo hostnamectl set-hostname n8n.grapelandisd.local"
+    }
+    # Prefer to run the VM on the primary pool instance
+    affinity_host = data.xenorchestra_pool.testhost.master
+    network {
+      network_id = data.xenorchestra_network.net.id
+    }
+    disk {
+      sr_id = "6f710bd3-cd24-d6ac-f7e9-8c076239172f"
+      name_label = "n8ntestserver"
+      size = 64424509440
+    }
+
+    tags = [
+      "Ubuntu",
+      "snapshot",
+    ]
+
+    // Override the default create timeout from 5 mins to 20.
+    timeouts {
+      create = "20m"
+    }
+}
